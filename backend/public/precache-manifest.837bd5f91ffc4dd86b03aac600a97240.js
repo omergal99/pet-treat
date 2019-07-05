b6299b9ac1ee20f08376a57ef4003a25e4850b6a
omergal99 <omergal99gm@gmail.com>
2019-07-05 05:31:25 +0300

    omer- upload heroku

---
+++ backend/public/precache-manifest.837bd5f91ffc4dd86b03aac600a97240.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8681f17514859c4bbf02ea7f01e16d53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3f75308cef2da15b3e7",
+    "url": "/static/css/main.eb7d1645.chunk.css"
+  },
+  {
+    "revision": "a2521cb0db8de938ec7b",
+    "url": "/static/js/2.cf6aab88.chunk.js"
+  },
+  {
+    "revision": "e3f75308cef2da15b3e7",
+    "url": "/static/js/main.7ca5ba32.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "56cb366910aa712773f8f2f7e7f42b9f",
+    "url": "/static/media/Merienda-Regular.56cb3669.ttf"
+  },
+  {
+    "revision": "849e67957ea4dd5edb38eedb9d5743f1",
+    "url": "/static/media/Quando-Regular.849e6795.ttf"
+  }
+]);

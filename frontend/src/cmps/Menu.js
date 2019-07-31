f5fbfb5311fd2bf2740a29677da7a51dbf340346
omergal99 <omergal99gm@gmail.com>
2019-07-31 19:44:57 +0300

    omer- first menu base css

--- frontend/src/cmps/Menu.js
+++ frontend/src/cmps/Menu.js
@@ -1,35 +1,46 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function Menu() {
+function Menu({ isOpen, onClose }) {
 
-  const toOpen = 'assets/img/general/icons/burger.png';
-  const toClose = 'assets/img/general/icons/burger-white.png';
-
-  let menuStatus = toClose;
-
-  const [isClose, toggleIsClose] = useState(true);
-
-  const toggleMenu = () => {
-    toggleIsClose(!isClose)
-    // if (menuStatus === toClose) {
-    //   console.log('111111')
-    //   menuStatus = 'assets/img/general/icons/burger.png';
-    // }else{
-    //   console.log('2222222')
-    //   menuStatus = toClose;
-    // }
+  const closeMenu = () => {
+    onClose();
   }
 
   return (
-    // <li className="nav-options" onClick={toggleMenu.bind(this)}>
-    <li className="nav-options" onClick={() => toggleMenu()}>
-      {isClose &&
-        <img src={toOpen} alt="Menu" />
-      }
-      {!isClose &&
-        <img src={toClose} alt="Menu" />
-      }
-    </li>
+    <div>
+      <div >
+        <div className="cover" style={{ display: isOpen ? '' : 'none' }} onClick={closeMenu.bind(this)}></div>
+        <div className="menu" style={{ right: isOpen ? '' : '-80vw' }}>
+          <div className="head flex flex-col">
+            <div className="flex">
+              <div className="dog-img">
+                <img src="../assets/img/dog/messi.jpg" alt="Messi" />
+              </div>
+              <div className="dog-details">
+                <label><span>name: </span>Messi</label>
+                <label><span>age: </span>10.5</label>
+                <label><span>species: </span>Golden and cocker spaniol</label>
+              </div>
+            </div>
+            <div className="dog-edit">
+              <button>Edit Details</button>
+            </div>
+          </div>
+          <div className="options">
+            <ul>
+              <div>Options</div>
+              <li>Gallery</li>
+              <li>The Family</li>
+              <li>Export Chat</li>
+              <hr />
+              <div>Acount</div>
+              <li>Settings</li>
+              <li>Logout</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- frontend/src/cmps/NavBar.js
+++ frontend/src/cmps/NavBar.js
@@ -1,27 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import Menu from './Menu';
 
-const NavBar = ({ currUserName }) => (
-  <nav className="Nav_menu">
-    <ul>
-      {!currUserName &&
-        <div className="link">
-          <NavLink to="/"
-            activeClassName="activeRoute">Sign Up</NavLink>
-        </div>
-      }
-      {currUserName &&
-        <div className="link flex space-between align-center">
-          <NavLink exact to="/" activeClassName="activeRoute">Home</NavLink>
-          <NavLink to="/chat" activeClassName="activeRoute">Chat</NavLink>
-          <NavLink to="/signup" activeClassName="activeRoute">Logout</NavLink>
-          <Menu />
-        </div>
-      }
-    </ul>
-  </nav>
-)
+function NavBar({ currUserName }) {
 
-export default NavBar;
+  const toOpen = 'assets/img/general/icons/burger.png';
+  const toClose = 'assets/img/general/icons/burger-white.png';
+
+  const [menuImg, toggleMenuImg] = useState(toOpen);
+
+  const toggleMenu = () => {
+    (menuImg === toClose) ? toggleMenuImg(toOpen) : toggleMenuImg(toClose);
+  }
+
+  return (
+    <nav className="Nav_menu">
+      <ul>
+        {!currUserName &&
+          <div className="link">
+            <NavLink to="/"
+              activeClassName="activeRoute">Sign Up</NavLink>
+          </div>
+        }
+        {currUserName &&
+          <div className="link flex space-between align-center">
+            <NavLink exact to="/" activeClassName="activeRoute">Home</NavLink>
+            <NavLink to="/chat" activeClassName="activeRoute">Chat</NavLink>
+            <NavLink to="/signup" activeClassName="activeRoute">Logout</NavLink>
+            <li className="nav-options" onClick={toggleMenu.bind(this)}>
+              <img src={menuImg} alt="Menu" />
+            </li>
+          </div>
+        }
+      </ul>
+      <Menu isOpen={(menuImg === toClose)} onClose={toggleMenu.bind(this)} />
+    </nav>
+  );
+}
+
+export default React.memo(NavBar)
